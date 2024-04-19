11ed3faae0ef53adab23b82655d370cb85ac9c1e
Shubzz-02 <19619948+Shubzz-02@users.noreply.github.com>
2024-04-19 05:25:19 +0000

    completed stage 6

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import socket
 from app.RedisProtocolParser import parse_protocol
 from threading import Thread
 
+simple_map = {}
+
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
@@ -22,6 +24,17 @@ def handle_client(client_sock):
             client_sock.sendall(resp.encode("utf-8"))
         elif parsed_data[0].lower() == 'echo':
             resp = f'${len(parsed_data[1])}\r\n{parsed_data[1]}\r\n'
+            client_sock.sendall(resp.encode("utf-8"))
+        elif parsed_data[0].lower() == 'set':
+            simple_map[parsed_data[1]] = parsed_data[2]
+            resp = '+OK\r\n'
+            client_sock.sendall(resp.encode("utf-8"))
+        elif parsed_data[0].lower() == 'get':
+            value = simple_map.get(parsed_data[1])
+            if value:
+                resp = f'${len(value)}\r\n{value}\r\n'
+            else:
+                resp = '$-1\r\n'
             client_sock.sendall(resp.encode("utf-8"))
         else:
             resp = '+skkep\r\n'
