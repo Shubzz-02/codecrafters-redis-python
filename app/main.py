fd21debd765efe36863160d21a4492efda1bf2a0
Shubzz-02 <19619948+Shubzz-02@users.noreply.github.com>
2024-04-22 18:41:10 +0000

    Disable psync command handling
    The change comments out the function call that handles the 'psync' command in main.py. This function was originally responsible for sending a response to the client side after parsing and handling a 'psync' request. However, the command is now skipped and an rdb file is directly sent instead.

--- app/main.py
+++ app/main.py
@@ -100,7 +100,7 @@ def process_command(parsed_data, server_config, client_sock):
     elif command == 'replconf':
         client_sock.sendall(handle_replica_command(parsed_data, server_config, client_sock).encode("utf-8"))
     elif command == 'psync':
-        client_sock.sendall(handle_psync_command(parsed_data, server_config).encode("utf-8"))
+        # client_sock.sendall(handle_psync_command(parsed_data, server_config).encode("utf-8"))
         # time.sleep(3)
         client_sock.sendall(send_rdb_file().encode("utf-8"))
     else:
